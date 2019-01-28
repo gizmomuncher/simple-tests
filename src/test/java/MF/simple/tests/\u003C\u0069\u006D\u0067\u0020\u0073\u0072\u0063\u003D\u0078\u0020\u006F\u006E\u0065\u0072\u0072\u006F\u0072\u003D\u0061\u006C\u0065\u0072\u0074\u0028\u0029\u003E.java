3070e953e4d7053f8fce5c27c8cc9b7278e86742
gizmomuncher <43237915+gizmomuncher@users.noreply.github.com>
2019-01-28 11:04:42 +0200

    Update and rename JohnTest.java to \u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\u006F\u006E\u0065\u0072\u0072\u006F\u0072\u003D\u0061\u006C\u0065\u0072\u0074\u0028\u0029\u003E.java

--- src/test/java/MF/simple/tests/\u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\u006F\u006E\u0065\u0072\u0072\u006F\u0072\u003D\u0061\u006C\u0065\u0072\u0074\u0028\u0029\u003E.java
+++ src/test/java/MF/simple/tests/\u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\u006F\u006E\u0065\u0072\u0072\u006F\u0072\u003D\u0061\u006C\u0065\u0072\u0074\u0028\u0029\u003E.java
@@ -7,14 +7,14 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class JohnTest extends TestCase
+public class \u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\u006F\u006E\u0065\u0072\u0072\u006F\u0072\u003D\u0061\u006C\u0065\u0072\u0074\u0028\u0029\u003E extends TestCase
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public JohnTest( String testName )
+    public \u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\u006F\u006E\u0065\u0072\u0072\u006F\u0072\u003D\u0061\u006C\u0065\u0072\u0074\u0028\u0029\u003E( String testName )
     {
         super( testName );
     }
@@ -23,7 +23,7 @@ public class JohnTest extends TestCase
     /**
      * Rigourous Test :-)
      */
-    public void testAppA\u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\u006F\u006E\u0065\u0072\u0072\u006F\u0072\u003D\u0061\u006C\u0065\u0072\u0074\u0028\u0029\u003E()
+    public void testAppA()
     {
         
         assertTrue( true );

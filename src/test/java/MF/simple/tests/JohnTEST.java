f4c2a572805b1eaa98824939b49a2445205069e6
gizmomuncher <43237915+gizmomuncher@users.noreply.github.com>
2019-01-28 10:05:07 +0200

    Add files via upload

---
+++ src/test/java/MF/simple/tests/JohnTEST.java
@@ -0,0 +1,52 @@
+package MF.simple.tests;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class JohnTest extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public JohnTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( JohnTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testAppA()
+    {
+        assertTrue( true );
+    }
+
+    public void testAppB()
+    {
+        assertTrue( true );
+    }
+
+    public void testAppC()
+    {
+        assertTrue( true );
+    }
+
+    public void testAlwaysFail()
+    {
+        assertTrue( true );
+    }
+}

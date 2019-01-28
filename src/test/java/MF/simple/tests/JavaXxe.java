16f9cf11a5c6ac7608d37c044195190fb1cea686
gizmomuncher <43237915+gizmomuncher@users.noreply.github.com>
2019-01-28 13:28:26 +0200

    Update JavaXxe.java

--- src/test/java/MF/simple/tests/JavaXxe.java
+++ src/test/java/MF/simple/tests/JavaXxe.java
@@ -1,12 +1,14 @@
 import java.net.URLConnection;
 
-public class Xxe(){
+public class Xxe
+{
 String url = "http://10.14.49.6:8000";
 String charset = "UTF-8";  // Or in Java 7 and later, use the constant: java.nio.charset.StandardCharsets.UTF_8.name()
 String param1 = "Hello";
 String param2 = "World!";
      
-     public void main(){
+     public void main()
+     {
 String query = String.format("param1=%s&param2=%s", 
      URLEncoder.encode(param1, charset), 
      URLEncoder.encode(param2, charset));

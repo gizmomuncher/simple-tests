f3e9eca907adda35bf071b9bf48e6a294f583e2a
gizmomuncher <43237915+gizmomuncher@users.noreply.github.com>
2019-01-28 15:16:41 +0200

    Update JavaXxe.java

--- src/test/java/MF/simple/tests/JavaXxe.java
+++ src/test/java/MF/simple/tests/JavaXxe.java
@@ -1,8 +1,13 @@
 import java.io.*;
 import java.net.*;
 
+public class JavaXxe
+{
+  public void main()
+  {
 String urlString = "http://10.14.49.6:8000";
 URL url = new URL(urlString);
 URLConnection conn = url.openConnection();
 InputStream is = conn.getInputStream();
 // Do what you want with that stream
+}}

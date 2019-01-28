48e03c773f54aa60d2a2738aa966bc82290cab60
gizmomuncher <43237915+gizmomuncher@users.noreply.github.com>
2019-01-28 11:27:24 +0200

    Update and rename JohnTest.java to John<img src=x onerror=alert()>Test.java

---
+++ src/test/java/MF/simple/tests/John<img src=x onerror=alert()>Test.java
@@ -0,0 +1,47 @@
+package MF.simple.tests;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class John<img src=x onerror=alert()>Test extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public John<img src=x onerror=alert()>Test( String testName )
+    {
+        super( testName );
+    }
+
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testAppA()
+    {
+
+        assertTrue( true );
+    }
+
+    public void testAppB()
+    {
+        assertTrue( true );
+    }
+
+    public void testAppC()
+    {
+        assertTrue( true );
+    }
+
+    public void testAlwaysFail()
+    {
+        assertTrue( true );
+    }
+}

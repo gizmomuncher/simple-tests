907905697a1bb30a0c51be72f28850c4600eacdf
gizmomuncher <43237915+gizmomuncher@users.noreply.github.com>
2019-01-28 15:14:00 +0200

    Update JavaXxe.java

--- src/test/java/MF/simple/tests/JavaXxe.java
+++ src/test/java/MF/simple/tests/JavaXxe.java
@@ -1,24 +1,8 @@
 import java.io.*;
 import java.net.*;
 
-public class c {
-
-   public static String getHTML(String urlToRead) throws Exception {
-      StringBuilder result = new StringBuilder();
-      URL url = new URL(urlToRead);
-      HttpURLConnection conn = (http://10.14.49.6:8000) url.openConnection());
-      conn.setRequestMethod("GET");
-      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-      String line;
-      while ((line = rd.readLine()) != null) {
-         result.append(line);
-      }
-      rd.close();
-      return result.toString();
-   }
-
-   public static void main(String[] args) throws Exception
-   {
-     System.out.println(getHTML(args[0]));
-   }
-}
+String urlString = "http://10.14.49.6:8000";
+URL url = new URL(urlString);
+URLConnection conn = url.openConnection();
+InputStream is = conn.getInputStream();
+// Do what you want with that stream

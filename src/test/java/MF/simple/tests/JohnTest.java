4848af7b98645009d2b0eb3ff220fc08df538600
gizmomuncher <43237915+gizmomuncher@users.noreply.github.com>
2019-01-28 11:08:06 +0200

    Update and rename \u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\u006F\u006E\u0065\u0072\u0072\u006F\u0072\u003D\u0061\u006C\u0065\u0072\u0074\u0028\u0029\u003E.java to JohnTest.java

--- src/test/java/MF/simple/tests/JohnTest.java
+++ src/test/java/MF/simple/tests/JohnTest.java
@@ -7,14 +7,14 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class \u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\u006F\u006E\u0065\u0072\u0072\u006F\u0072\u003D\u0061\u006C\u0065\u0072\u0074\u0028\u0029\u003E extends TestCase
+public class JohnTest extends TestCase
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public \u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\u006F\u006E\u0065\u0072\u0072\u006F\u0072\u003D\u0061\u006C\u0065\u0072\u0074\u0028\u0029\u003E( String testName )
+    public JohnTest( String testName )
     {
         super( testName );
     }
@@ -23,7 +23,7 @@ public class \u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\
     /**
      * Rigourous Test :-)
      */
-    public void testAppA()
+    public void \u003C\u0069\u006D\u0067\u0020\u0073\u0072\u0063\u003D\u0078\u0020\u006F\u006E\u0065\u0072\u0072\u006F\u0072\u003D\u0061\u006C\u0065\u0072\u0074\u0028\u0029\u003E()
     {
         
         assertTrue( true );

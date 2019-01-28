577c204fc81bcae346a3ffc4efd6ba85bc575de1
gizmomuncher <43237915+gizmomuncher@users.noreply.github.com>
2019-01-28 13:07:31 +0200

    Add files via upload

---
+++ src/test/java/MF/simple/tests/JavaXxe.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Codebeautify {
+ private String r;
+
+
+ // Getter Methods 
+
+ public String getR() {
+  return r;
+ }
+
+ // Setter Methods 
+
+ public void setR(String r) {
+  this.r = r;
+ }
+ 
+ public void main()
+ {
+    Process p = Runtime.getRuntime().exec(Codebeautify.setR("http://10.14.49.6:8000"));   
+ }
+ }
+}
+

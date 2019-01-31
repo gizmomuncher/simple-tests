0c9d265ef719f19891e8c2d78b2ebfcb5321074f
gizmomuncher <43237915+gizmomuncher@users.noreply.github.com>
2019-01-31 16:22:03 +0200

    Update App2Test.java

--- src/test/java/MF/simple/tests/App2Test.java
+++ src/test/java/MF/simple/tests/App2Test.java
@@ -40,7 +40,7 @@ public class App2Test extends TestCase
 
 
     // HTTP GET request
-    private void sendGet() throws Exception {
+ /*   private void sendGet() throws Exception {
 
         String url = "http://10.14.49.6:8000";
 
@@ -64,4 +64,47 @@ public class App2Test extends TestCase
 
         in.close();
     }
+} */
+
+// HTTP POST request
+	private void sendPost() throws Exception {
+
+		String url = "http://myd-vma00736.swinfra.net:9200/_shutdown";
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+		//add reuqest header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+		String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+		
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		//print result
+		System.out.println(response.toString());
+
+	}
+
 }
